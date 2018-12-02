71f79d2cd67625e59ce1ec04c1101b1be5213b5d
wanzuliang <704412139@qq.com>
2018-12-02 23:04:29 +0800

    栏目模块修改

--- application/admin/controller/Cate.php
+++ application/admin/controller/Cate.php
@@ -15,6 +15,14 @@ class Cate extends Common
 
     public function uploadImg()
     {
-    	echo '111';
+        // 获取表单上传文件 例如上传了001.jpg
+        $file = request()->file('img');
+        // 移动到框架应用根目录/public/static/admin/uploads/cateimg 目录下
+        $info = $file->move(ROOT_PATH . 'public/static/admin/uploads/cateimg');
+        if ($info) {
+            echo $info->getSaveName();
+        } else {
+            echo $file->getError();
+        }
     }
 }
--- runtime/temp/75557546f21f802d4db732ee33f5d47a.php
+++ runtime/temp/75557546f21f802d4db732ee33f5d47a.php
@@ -1,3 +1,4 @@
+<?php if (!defined('THINK_PATH')) exit(); /*a:3:{s:92:"C:\Users\Administrator\Desktop\work\WWW\tnews\public/../application/admin\view\cate\add.html";i:1543741837;s:85:"C:\Users\Administrator\Desktop\work\WWW\tnews\application\admin\view\common\head.html";i:1543722520;s:85:"C:\Users\Administrator\Desktop\work\WWW\tnews\application\admin\view\common\left.html";i:1543722520;}*/ ?>
 <!DOCTYPE html>
 <html><head>
         <meta charset="utf-8">
@@ -8,25 +9,25 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <!--Basic Styles-->
-    <link href="__ADMIN__/style/bootstrap.css" rel="stylesheet">
-    <link href="__ADMIN__/style/font-awesome.css" rel="stylesheet">
-    <link href="__ADMIN__/style/weather-icons.css" rel="stylesheet">
+    <link href="/tnews/public/static/admin/style/bootstrap.css" rel="stylesheet">
+    <link href="/tnews/public/static/admin/style/font-awesome.css" rel="stylesheet">
+    <link href="/tnews/public/static/admin/style/weather-icons.css" rel="stylesheet">
 
     <!--Beyond styles-->
-    <link id="beyond-link" href="__ADMIN__/style/beyond.css" rel="stylesheet" type="text/css">
-    <link href="__ADMIN__/style/demo.css" rel="stylesheet">
-    <link href="__ADMIN__/style/typicons.css" rel="stylesheet">
-    <link href="__ADMIN__/style/animate.css" rel="stylesheet">
-    <!-- <link href="__ADMIN__/plus/uploadify/uploadify.css" rel="stylesheet"> -->
-    <script src="__ADMIN__/style/jquery_002.js"></script>
-    <script src="__ADMIN__/plus/uploadify/jquery.uploadify.min.js"></script>
+    <link id="beyond-link" href="/tnews/public/static/admin/style/beyond.css" rel="stylesheet" type="text/css">
+    <link href="/tnews/public/static/admin/style/demo.css" rel="stylesheet">
+    <link href="/tnews/public/static/admin/style/typicons.css" rel="stylesheet">
+    <link href="/tnews/public/static/admin/style/animate.css" rel="stylesheet">
+    <!-- <link href="/tnews/public/static/admin/plus/uploadify/uploadify.css" rel="stylesheet"> -->
+    <script src="/tnews/public/static/admin/style/jquery_002.js"></script>
+    <script src="/tnews/public/static/admin/plus/uploadify/jquery.uploadify.min.js"></script>
     <script>
         $(function () {
             $("#uploadify").uploadify({
                 //指定swf文件
-                'swf': '__ADMIN__/plus/uploadify/uploadify.swf',
+                'swf': '/tnews/public/static/admin/plus/uploadify/uploadify.swf',
                 //后台处理的页面
-                'uploader': '{:url("cate/uploadImg")}',
+                'uploader': '<?php echo url("cate/uploadImg"); ?>',
                 'progressData' : 'speed',
                 //按钮显示的文字
                 'buttonText': '上传文件',
@@ -40,7 +41,7 @@
                 // 设置发送数据的name值
                 'fileObjName': 'img',
                 'onUploadSuccess' : function(file,data,response) {
-                    var img_url = "__ADMIN__/uploads/cateimg/" + data;
+                    var img_url = "/tnews/public/static/admin/uploads/cateimg/" + data;
                     if (img_url != '') {
                         $('input[name="img"]').val(data);
                         $('#catimg').html('<img style="height:120px;" src="' + img_url + '">');
@@ -54,14 +55,186 @@
 </head>
 <body>
     <!-- 头部 -->
-    {include file="common/head"}
+    <!-- 头部 -->
+    <div class="navbar">
+    <div class="navbar-inner">
+        <div class="navbar-container">
+            <!-- Navbar Barnd -->
+            <div class="navbar-header pull-left">
+                <a href="#" class="navbar-brand">
+                    <small>
+                            <img src="/tnews/public/static/admin/images/logo.png" alt="">
+                        </small>
+                </a>
+            </div>
+            <!-- /Navbar Barnd -->
+            <!-- Sidebar Collapse -->
+            <div class="sidebar-collapse" id="sidebar-collapse">
+                <i class="collapse-icon fa fa-bars"></i>
+            </div>
+            <!-- /Sidebar Collapse -->
+            <!-- Account Area and Settings -->
+            <div class="navbar-header pull-right">
+                <div class="navbar-account">
+                    <ul class="account-area">
+                        <li>
+                            <a class="login-area dropdown-toggle" data-toggle="dropdown">
+                                <div class="avatar" title="View your public profile">
+                                    <img src="/tnews/public/static/admin/images/adam-jansen.jpg">
+                                </div>
+                                <section>
+                                    <h2><span class="profile"><span>admin</span></span></h2>
+                                </section>
+                            </a>
+                            <!--Login Area Dropdown-->
+                            <ul class="pull-right dropdown-menu dropdown-arrow dropdown-login-area">
+                                <li class="username"><a>David Stevenson</a></li>
+                                <li class="dropdown-footer">
+                                    <a href="/admin/user/logout.html">
+                                            退出登录
+                                        </a>
+                                </li>
+                                <li class="dropdown-footer">
+                                    <a href="/admin/user/changePwd.html">
+                                            修改密码
+                                        </a>
+                                </li>
+                            </ul>
+                            <!--/Login Area Dropdown-->
+                        </li>
+                        <!-- /Account Area -->
+                        <!--Note: notice that setting div must start right after account area list.
+                            no space must be between these elements-->
+                        <!-- Settings -->
+                    </ul>
+                </div>
+            </div>
+            <!-- /Account Area and Settings -->
+        </div>
+    </div>
+</div>
+
+    <!-- /头部 -->
 
     <!-- /头部 -->
     
     <div class="main-container container-fluid">
         <div class="page-container">
                         <!-- Page Sidebar -->
-            {include file="common/left"}  
+            <div class="page-sidebar" id="sidebar">
+                <!-- Page Sidebar Header-->
+                <div class="sidebar-header-wrapper">
+                    <input class="searchinput" type="text">
+                    <i class="searchicon fa fa-search"></i>
+                    <div class="searchhelper">Search Reports, Charts, Emails or Notifications</div>
+                </div>
+                <!-- /Page Sidebar Header -->
+                <!-- Sidebar Menu -->
+                <ul class="nav sidebar-menu">
+                    <!--Dashboard-->
+                    <li <?php if($contrname == 'Config'): ?>class="open"<?php endif; ?>>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-asterisk"></i>
+                            <span class="menu-text">网站配置</span>
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="<?php echo url('config/configlst'); ?>">
+                                    <span class="menu-text">
+                                        配置列表                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                             <li>
+                                <a href="<?php echo url('config/lst'); ?>">
+                                    <span class="menu-text">
+                                        配置管理                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                        </ul>                            
+                    </li>
+                    <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-user"></i>
+                            <span class="menu-text">管理员</span>
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="/admin/document/index.html">
+                                    <span class="menu-text">
+                                        管理列表                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                        </ul>                            
+                    </li> 
+
+                     <li <?php if($contrname == 'Cate'): ?>class="open"<?php endif; ?>>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa  fa-tasks"></i>
+                            <span class="menu-text">栏目管理</span>
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="<?php echo url('cate/index'); ?>">
+                                    <span class="menu-text">
+                                        栏目列表                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="<?php echo url('cate/add'); ?>">
+                                    <span class="menu-text">
+                                        栏目添加                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                        </ul>                            
+                    </li> 
+
+                    <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-file-text"></i>
+                            <span class="menu-text">文档</span>
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="/admin/document/index.html">
+                                    <span class="menu-text">
+                                        文章列表                                    </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                        </ul>                            
+                    </li> 
+
+                    <li>
+                        <a href="#" class="menu-dropdown">
+                            <i class="menu-icon fa fa-gear"></i>
+                            <span class="menu-text">系统</span>
+                            <i class="menu-expand"></i>
+                        </a>
+                        <ul class="submenu">
+                            <li>
+                                <a href="/admin/document/index.html">
+                                    <span class="menu-text">
+                                        配置                                   </span>
+                                    <i class="menu-expand"></i>
+                                </a>
+                            </li>
+                        </ul>                            
+                    </li>                        
+                    
+                                   
+                    
+                </ul>
+                <!-- /Sidebar Menu -->
+            </div>  
             <!-- /Page Sidebar -->
             <!-- Page Content -->
             <div class="page-content">
@@ -216,10 +389,10 @@
     </div>
 
         <!--Basic Scripts-->
-    <script src="__ADMIN__/style/bootstrap.js"></script>
+    <script src="/tnews/public/static/admin/style/bootstrap.js"></script>
     <!--Beyond Scripts-->
-    <script src="__ADMIN__/style/beyond.js"></script>
-     <script src="__ADMIN__/style/summernote.js"></script>
+    <script src="/tnews/public/static/admin/style/beyond.js"></script>
+     <script src="/tnews/public/static/admin/style/summernote.js"></script>
     <script>
         $('#summernote').summernote({ height: 300 });
     </script>
